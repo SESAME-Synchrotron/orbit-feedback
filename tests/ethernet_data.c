d7ee69f944b2a8b9a57a88f0addb3c53aaf43a5c
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2022-01-03 16:28:25 +0200

    tests/ethernet

--- tests/ethernet_data.c
+++ tests/ethernet_data.c
@@ -19,23 +19,25 @@
 #define PSC_CONNECT	4
 #define PSC_POLL	5
 
-#define UDP_PACKET_LENGTH		10
-#define UDP_PACKET_STATUS_RAW  	0x0000
-#define COMMAND_READ			0x0001
-#define COMMAND_WRITE			0x0002
-#define PS_ADDRESS_SHIFT		14
-#define ADDRESS_PRIORITY		0x2 | (1 << PS_ADDRESS_SHIFT)
-#define ADDRESS_ILOAD			153 | (1 << PS_ADDRESS_SHIFT)
-#define ADDRESS_SET_REF			175 | (1 << PS_ADDRESS_SHIFT)
-#define ETHERNET_ENABLE			0x1000000
-#define POLL_TIMEOUT			1000
+#define UDP_PACKET_LENGTH	10
+#define PACKET_STATUS_RAW  	0x0000
+#define COMMAND_READ		0x0001
+#define COMMAND_WRITE		0x0002
+#define PS_ADDRESS_SHIFT	14
+#define ADDRESS_PRIORITY	0x2 | (1 << PS_ADDRESS_SHIFT)
+#define ADDRESS_ILOAD		153 | (1 << PS_ADDRESS_SHIFT)
+#define ADDRESS_SET_REF		175 | (1 << PS_ADDRESS_SHIFT)
+#define ETHERNET_ENABLE		0x1000000
+#define POLL_TIMEOUT		1000
+#define IP_ADDRESS			"10.2.2.33"
+#define PORT				9322
 
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t  u8;
 typedef int32_t  i32;
-typedef struct pollfd pollfd
+typedef struct pollfd pollfd;
 
 typedef struct
 {
@@ -57,9 +59,71 @@ typedef struct
 	packet_t eth;
 } psc_t;
 
+psc_t* psc_init(char* ip, u16 port);
+int psc_read(psc_t* device, float* value);
+int psc_write(psc_t* device);
+
 int main(int argc, char** argv)
 {
+	float  sampling = 10;
+	float  v1, v2;
+	float  t;
+	float  amplitude = 1;
+	float  frequency = 1;
+	float  increment;
+	float  _2pi_f;
+	u32    delay_us;
+	u32    duration;
+	i32    cycles = 1;
+	struct timespec start, end;
+	clockid_t id = CLOCK_MONOTONIC;
 
+	psc_t* psc;
+	psc = (psc_t*) psc_init(IP_ADDRESS, PORT);
+	if(!psc)
+	{
+		printf("Null PSC\n");
+		return 0;
+	}
+
+	psc->tx.address = ADDRESS_PRIORITY;
+	psc->tx.data = (u32) ETHERNET_ENABLE;
+	psc_write(psc);
+	psc->tx.address = ADDRESS_SET_REF;
+	psc->tx.data = 0;
+
+	if(argv[1])
+		sampling = atof(argv[1]);
+	if(argv[2])
+		frequency = atof(argv[2]);
+	if(argv[3])
+		cycles = atoi(argv[3]);
+	if(argv[4])
+		amplitude = atof(argv[4]);
+
+	delay_us = sampling * 1000 - 760;
+	increment = sampling / 1000.0;
+	_2pi_f = 2 * M_PI * frequency;
+	t = 0;
+	while( t <= (cycles / frequency) + sampling / 1000.0 )
+	{
+		clock_gettime(id, &start);
+		v1 = amplitude * sin(_2pi_f * t);
+		psc->tx.data = *(u32*)&v1;
+		psc_write(psc);
+		usleep(delay_us);
+		psc_read(psc, &v2);
+		t += increment;
+		clock_gettime(id, &end);
+		duration = (end.tv_sec - start.tv_sec)*1E9 + (end.tv_nsec - start.tv_nsec);
+		/* printf("Duration: %010.3f ms\n", duration / 1E6); */
+		printf("%f %f\n", v1, v2);
+	}
+
+	psc->tx.address = ADDRESS_PRIORITY;
+	psc->tx.data = 0;
+	psc_write(psc);
+	close(psc->fd);
 	return 0;
 }
 
@@ -84,12 +148,12 @@ psc_t* psc_init(char* ip, u16 port)
 	if(status < 0)
 		return NULL;
 
-	device->rx.status  = (u16) UDP_PACKET_STATUS_RAW;
+	device->rx.status  = (u16) PACKET_STATUS_RAW;
 	device->rx.command = (u16) COMMAND_READ;
 	device->rx.address = (u16) ADDRESS_ILOAD;
 	device->rx.data    = (u32) 0;
 
-	device->tx.status  = (u16) UDP_PACKET_STATUS_RAW;
+	device->tx.status  = (u16) PACKET_STATUS_RAW;
 	device->tx.command = (u16) COMMAND_WRITE;
 	device->tx.address = (u16) ADDRESS_SET_REF;
 	device->tx.data    = (u32) 0;
